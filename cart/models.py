1a38542f3e807e24b89b371d4e6cfb47a871fc85
alireza841384 <alireza.m.841384@gmail.com>
2025-10-30 23:12:54 +0330

    feat(cart):add Cart_Item model and change Product model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from accounts.models import User
+from products.models import Product
 # Create your models here.
+
+
+def get_unkown_product():
+    product = Product.objects.get_or_create(name="nonexistent product",
+                                            description="This product is no longer available.",
+                                            balance=0, price=0, category=None)
+    return product
+
+
+class Cart_Item(models.Model):
+    product = models.ForeignKey(
+        Product, related_name="cart_items", on_delete=models.SET(get_unkown_product()))
+    # shoping_cart=models.ForeignKey(,related_name="cart_items",on_delete=models.CASCADE)
+    number = models.IntegerField()
+    total = models.BigIntegerField()
+    date = models.DateTimeField()
