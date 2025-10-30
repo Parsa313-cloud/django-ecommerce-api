ae44abadc2c3893ef8292876e095d7c343547384
Parsa313-cloud <haroonip313@gmail.com>
2025-10-30 15:42:43 +0330

    Add .gitignore

--- accounts/models.py
+++ accounts/models.py
@@ -2,20 +2,19 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
 
-class Profile(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    address = models.TextField(null=True)
-    phone_number = PhoneNumberField(blank=True, null=True)
-    def __str__(self):
-        return self.first_name
-
-
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    profile = models.OneToOneField(Profile , on_delete=models.CASCADE)
-    #orders
-    #profile
+
     def __str__(self):
         return self.email
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    phone_number = PhoneNumberField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
