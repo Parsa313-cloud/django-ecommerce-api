eb2ae73a44dba2afadee729c01db07cad48c96de
Parsa313-cloud <haroonip313@gmail.com>
2025-11-01 00:49:32 +0330

    refactor(account):repair model serializers for User & Profile Models

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,17 +1,29 @@
 from rest_framework import serializers
-
-from .models import User, Profile
-
+from .models import Profile, User
 
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.HyperlinkedRelatedField(
+        view_name='user-detail',
+        lookup_field='email',
+        read_only=True,
+    )
+
     class Meta:
         model = Profile
-        fields = "__all__"
-
-
+        fields = ['url', 'user', 'first_name', 'last_name', 'address', 'phone_number', 'slug', 'created_at', 'updated_at']
+        extra_kwargs = {
+            'url': {'view_name': 'profile-detail', 'lookup_field': 'slug'}
+        }
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    profile = ProfileSerializer()
+    profile = serializers.HyperlinkedRelatedField(
+        view_name='profile-detail',
+        lookup_field='slug',
+        read_only=True
+    )
 
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ['url', 'username', 'email', 'profile']
+        extra_kwargs = {
+            'url': {'view_name': 'user-detail', 'lookup_field': 'email'}
+        }
