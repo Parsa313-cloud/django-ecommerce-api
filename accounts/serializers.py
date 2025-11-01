5fb448b2887df4bfc9e681be599da70c1963d23e
alireza841384 <alireza.m.841384@gmail.com>
2025-11-01 22:13:43 +0330

    fix(accounts):add relation with OrderItems for UserSerializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Profile, User
 
+
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     user = serializers.HyperlinkedRelatedField(
         view_name='user-detail',
@@ -10,20 +11,29 @@ class ProfileSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Profile
-        fields = ['url', 'user', 'first_name', 'last_name', 'address', 'phone_number', 'slug', 'created_at', 'updated_at']
+        fields = ['url', 'user', 'first_name', 'last_name', 'address',
+                  'phone_number', 'slug', 'created_at', 'updated_at']
         extra_kwargs = {
             'url': {'view_name': 'profile-detail', 'lookup_field': 'slug'}
         }
+
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     profile = serializers.HyperlinkedRelatedField(
         view_name='profile-detail',
         lookup_field='slug',
         read_only=True
     )
+    orderItems = serializers.HyperlinkedRelatedField(
+        view_name="orderitem-detail",
+        lookup_filed='public_id',
+        read_only=True,
+        many=True
+    )
 
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'profile']
+        fields = ['url', 'username', 'email', 'profile', 'orderItems']
         extra_kwargs = {
             'url': {'view_name': 'user-detail', 'lookup_field': 'email'}
         }
