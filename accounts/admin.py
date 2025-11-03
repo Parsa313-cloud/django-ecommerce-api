46f64019108b291f32eea077e09d289cfecffa44
alireza841384 <alireza.m.841384@gmail.com>
2025-11-03 21:05:24 +0330

    feat(accounts):add model admin for User & Profile

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import User, Profile
 
 # Register your models here.
+
+
+class ProfileInline(admin.TabularInline):
+    model = Profile
+    extra = 0
+
+
+@admin.register(User)
+class UserAmin(admin.ModelAdmin):
+    list_disply = ["email"]
+    inlines = [ProfileInline]
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["user", "first_name", "last_name", "address",
+                    "phone_number", "slug", "created_at", "update_at"]
+    search_fields = ["first_name", "last_name", "slug"]
