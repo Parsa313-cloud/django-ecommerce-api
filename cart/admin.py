e8053221c5199e7f013ce09d015162b25778a6ee
alireza841384 <alireza.m.841384@gmail.com>
2025-11-03 21:20:27 +0330

    feat(cart):add model admin for ShoppingCart & CartItem & OrderItem

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
-
+from .models import ShoppingCart, CartItem, OrderItem
+from products.models import Product
 # Register your models here.
+
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 1
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ["user"]
+    inlines = [CartItemInline]
+
+
+class ProductInline(admin.TabularInline):
+    model = Product
+    extra = 0
+    can_delete = False
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['number', "public_id", "date", "shopping_cart"]
+    inlines = [ProductInline]
+    search_fields = ["public_id", "date"]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ["name", "description",
+                    "category", "number", "price", "time"]
+    inlines = [Product]
