ebd43abb38649198ea3d611c47d251ed2f068999
Parsa313-cloud <haroonip313@gmail.com>
2025-11-20 23:05:18 +0330

    feat(account):add LoginView

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
-from rest_framework.views import APIView, Response
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework import permissions, status, viewsets
 from rest_framework.exceptions import PermissionDenied
 from django.db import transaction
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.urls import reverse
+
 
 
 from .serializers import UserSignUpSerializer, ProfileSerializer, UserSerializer
@@ -24,7 +28,7 @@ class SignUpView(APIView):
             "username": user.username,
             "email": user.email,
             "message": "User created",
-            "login_url": "auth/login"
+            "login_url": reverse("login"),
         }, status=status.HTTP_201_CREATED)
 
 
@@ -71,14 +75,47 @@ class UserViewSet(viewsets.ModelViewSet):
         if user.is_staff:
             return User.objects.all()
 
-        return User.objects.filter(user=user)
+        return User.objects.filter(id=user.id)
 
     def get_object(self):
         obj = super().get_object()
         user = self.request.user
         if user.is_staff:
             return obj
-        if obj.user != user:
-            raise PermissionDenied(
-                "You don’t have permission to access this user.")
+        if obj != user:
+            raise PermissionDenied("You don’t have permission to access this user.")
+
         return obj
+
+class LoginView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        if not username or not password:
+            return Response(
+                {"error": "Please provide username and password."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is None:
+            return Response(
+                {"error": "Incorrect username or password."},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+        if not user.is_active:
+            return Response({"error": "User account is disabled."}, status=status.HTTP_403_FORBIDDEN)
+
+        refresh = RefreshToken.for_user(user)
+        data = {
+            "username": user.username,
+            "email": user.email,
+            "access": str(refresh.access_token),
+            "refresh": str(refresh),
+            "message": "User successfully logged in."
+        }
+        return Response(data, status=status.HTTP_200_OK)
